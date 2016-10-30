4b7a71eb6bbbb4c23f01cf7e1de3112792e1bdd7
Rafael Grillo <grillorafael@gmail.com>
2016-10-30 15:08:38 +0000

    Improve workflow

--- js/file-to-canvas.js
+++ js/file-to-canvas.js
@@ -1,50 +1,75 @@
-  //
-  // Disable workers to avoid yet another cross-origin issue (workers need the URL of
-  // the script to be loaded, and dynamically loading a cross-origin script does
-  // not work)
-  //
-  PDFJS.disableWorker = true;
-  //
-  // Asynchronous download PDF as an ArrayBuffer
-  //
-  var pdfInput = document.getElementById('pdf');
-  //
-  pdfInput.onchange = function(ev) {
-      if (file = document.getElementById('pdf').files[0]) {
-          fileReader = new FileReader();
+class PDFLoader {
+    constructor(inputElement, previewWrapper) {
+        //TODO: Verify if it is really necessary to disable this
+        // PDFJS.disableWorker = true;
 
-          fileReader.onload = function(ev) {
-              console.log(ev);
-              PDFJS.getDocument(fileReader.result).then(function getPdfHelloWorld(pdf) {
-                  numpag = pdf.numPages;
-                  console.log(`Number of Pages ${numpag}`);
-                  pdf.getPage(count).then(function getPageHelloWorld(page) {
-                      var scale = 2.5;
-                      var viewport = page.getViewport(scale);
-                      //
-                      // Prepare canvas using PDF page dimensions
-                      //
-                      var canvas = document.getElementById('the-canvas');
-                      var context = canvas.getContext('2d');
-                      canvas.height = viewport.height;
-                      canvas.width = viewport.width;
-                      //
-                      // Render PDF page into canvas context
-                      //
-                      var task = page.render({
-                          canvasContext: context,
-                          viewport: viewport
-                      })
-                      task.promise.then(function() {
-                          var canvass = document.getElementById('the-canvas');
-                          recognizeFile(canvas.toDataURL('image/jpeg'));
-                          count++;
-                      });
-                  });
-              }, function(error) {
-                  console.log(error);
-              });
-          };
-          fileReader.readAsArrayBuffer(file);
-      }
-  }
+        this.inputElement = inputElement;
+        this.previewWrapper = previewWrapper;
+    }
+    loadFile(ev) {
+        return new Promise((resolve, reject) => {
+            console.info('PDF Input Change');
+            let file = this.inputElement.files[0];
+            if (file) {
+                console.info('PDF Input File Available');
+                let fileReader = new FileReader();
+                fileReader.onload = (ev) => this.process(fileReader).then(resolve, reject);
+                fileReader.readAsArrayBuffer(file);
+            } else console.info('PDF Input File Unavailable');
+        });
+    }
+    process(fileReader) {
+        console.info(`Processing FileReader result`);
+
+        return PDFJS.getDocument(fileReader.result)
+            .then(loadPDF.bind(this))
+            .then(previewPages.bind(this));
+
+        function loadPDF(pdf) {
+            console.info(`PDFJS File parsed with ${pdf.numPages} pages`);
+            return new Promise((resolve, reject) => {
+                let pagePromises = [];
+                let canvasElements = [];
+
+                for (let i = 1; i <= pdf.numPages; i++) {
+                    pagePromises.push(pdf.getPage(i));
+                    let canvasPreview = document.createElement('canvas');
+                    canvasPreview.classList.add('canvas-preview');
+                    canvasPreview.setAttribute('page', `${i}`);
+                    canvasElements.push(canvasPreview);
+                }
+
+                Promise.all(pagePromises).then(pages => {
+                    resolve({pages, canvasElements});
+                }, reject);
+            });
+        }
+
+        function previewPages(data) {
+            let {pages, canvasElements} = data;
+            return new Promise((resolve, reject) => {
+                console.info('Start page preview');
+                let scale = 2.5;
+
+                let pagePromises = pages.map((p, i) => {
+                    let viewport = p.getViewport(scale);
+                    let canvas = canvasElements[i];
+                    let canvasContext = canvas.getContext('2d');
+
+                    canvas.height = viewport.height;
+                    canvas.width = viewport.width;
+
+                    let task = p.render({ canvasContext, viewport });
+
+                    return task.promise;
+                });
+
+                Promise.all(pagePromises).then(() => {
+                    canvasElements.forEach(e => this.previewWrapper.append(e));
+                    let imageData = canvasElements.map(e => e.toDataURL('image/jpeg'));
+                    resolve(imageData);
+                }, reject);
+            });
+        }
+    }
+}
